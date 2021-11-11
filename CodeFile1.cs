24d01627c30afb63ee9119c5a53bc947aae64a83
stas.marax@gmail.com <stas.marax@gmail.com>
2021-11-11 23:49:04 +0800

    Добавьте файлы проекта.

---
+++ CodeFile1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4GUI
+
+{
+    public class Gadjets
+    {
+        public static Random rnd = new Random();
+        public int rasmer = 0;
+        public virtual String GetInfo()
+        {
+            var str = String.Format("\nРазмер дисплея ={0}", this.rasmer);
+            return str;
+        }
+    }
+    public enum NoutbukType { Hp, Lenovo, Acer };
+    public class Noutbuk : Gadjets
+    {
+        public NoutbukType type = NoutbukType.Hp;
+        public int volumeHard = 0;
+        public int quantityCore = 0;
+        public bool rgb = false;
+        public override String GetInfo()
+        {
+            var str = "";
+            str += String.Format("\nФирма ноутбука: {0}", this.type);
+            str += base.GetInfo();
+            str += String.Format("\nОбъём жесткого диска: {0} Тб", this.volumeHard);
+            str += String.Format("\nКоличество ядер: {0}", this.quantityCore);
+            str += String.Format("\nНаличие подсветки: {0}", this.rgb);
+            return str;
+        }
+        public static Noutbuk Generate()
+        {
+            return new Noutbuk
+            {
+                rasmer = 10+ rnd.Next() % 15,
+                volumeHard =1+ rnd.Next() % 2,
+                quantityCore =rnd.Next() % 12,
+                rgb = rnd.Next() % 2 == 0,
+                type = (NoutbukType)rnd.Next(3)
+            };
+        }
+    }
+    public enum PlanshetType { Prestige, Huawei, Meizu };
+    public class Planshet : Gadjets
+        {
+            public PlanshetType type = PlanshetType.Prestige;
+            public int dpi = 0;
+            public bool camera = false;
+            public override String GetInfo()
+            {
+                var str = "";
+                str += String.Format("\nФирма планшета: {0}", this.type);
+                str += base.GetInfo();
+                str += String.Format("\nКоличество dpi экрана: {0}", this.dpi);
+                str += String.Format("\nНаличие камеры: {0}", this.camera);
+                return str;
+            }
+            public static Planshet Generate()
+            {
+                return new Planshet
+                {
+                    rasmer = 10 + rnd.Next() % 16,
+                    dpi = 300 + rnd.Next() %1000, 
+                    camera = rnd.Next() % 2 == 0,
+                    type = (PlanshetType)rnd.Next(3)
+                };
+
+            }
+        }
+    public enum SmartfonType { Samsung, Apple, Honor };
+    public class Smartfon : Gadjets
+        {
+            public SmartfonType type = SmartfonType.Samsung;
+            public int quantitySim = 0;
+            public int quantityMgps = 0;
+            public bool battery = false;
+            public override String GetInfo()
+            {
+                var str = "";
+                str += String.Format("\nФирма смартфона: {0}", this.type);
+                str += base.GetInfo();
+                str += String.Format("\nКоличество слотов для Sim-карт: {0}", this.quantitySim);
+                str += String.Format("\nКоличество мегапикселей: {0}", this.quantityMgps);
+                str += String.Format("\nСъемная батарея: {0}", this.battery);
+                return str;
+            }
+            public static Smartfon Generate()
+            {
+                    return new Smartfon
+                    {
+                        rasmer =8+ rnd.Next() % 8,
+                        quantitySim = 1 + rnd.Next() % 2,
+                        quantityMgps = 12 + rnd.Next() % 48, 
+                        battery = rnd.Next() % 2 == 0,
+                        type = (SmartfonType)rnd.Next(3)
+                    };
+            }
+
+        }
+    }
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4GUI
+{
+    public partial class Form1 : Form
+    {
+        List<Gadjets> GadjetsList = new List<Gadjets>();
+        public Form1()
+        {
+            InitializeComponent();
+            ShowInfo();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.GadjetsList.Clear();
+            var rnd = new Random();
+            for (var i = 0; i < 10; ++i)
+            {
+                switch (rnd.Next() % 3) 
+                {
+                    case 0:
+                        this.GadjetsList.Add(Noutbuk.Generate());
+                        break;
+                    case 1:
+                        this.GadjetsList.Add(Planshet.Generate());
+                        break;
+                    case 2:
+                        this.GadjetsList.Add(Smartfon.Generate());
+                        break;
+
+                }
+            }
+            ShowInfo();
+        }
+            private void ShowInfo()
+            {
+                int noutbukCount = 0;
+                int planshetCount = 0;
+                int smartfonCount = 0;
+                foreach (var gadjet in this.GadjetsList)
+                {
+                    if (gadjet is Noutbuk)
+                    {
+                        noutbukCount += 1;
+                    }
+                    else if (gadjet is Planshet)
+                    {
+                        planshetCount += 1;
+                    }
+                    else if (gadjet is Smartfon)
+                    {
+                        smartfonCount += 1;
+                    }
+                }
+                richTextBox2.Text = "Нтбк\tПлншт\tСмртфн";
+                richTextBox2.Text += "\n";
+                richTextBox2.Text += String.Format("{0}\t{1}\t{2}", noutbukCount, planshetCount, smartfonCount);
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.GadjetsList.Count == 0)
+            {
+                richTextBox1.Text = "Пусто Q_Q";
+                return;
+            }
+            var fruit = this.GadjetsList[0];
+            this.GadjetsList.RemoveAt(0);
+            richTextBox1.Text = fruit.GetInfo();
+            ShowInfo();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
